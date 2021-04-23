2bc574cc24d10d50148d3b2ed349956ee2377f90
Gift <gkenny896@gmail.com>
2021-04-23 07:50:05 +0100

    ...

---
+++ src/components/home/navbar/Navbar.js
@@ -0,0 +1,15 @@
+import { FaHome } from 'react-icons/fa';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </nav>
+    );
+};
+
+export default Navbar;
