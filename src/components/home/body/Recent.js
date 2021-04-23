3c0eca4456c310de3db10e9a9791af29b7805a75
Gift <gkenny896@gmail.com>
2021-04-23 12:56:23 +0100

    project concluded

--- src/components/home/body/Recent.js
+++ src/components/home/body/Recent.js
@@ -1,29 +1,31 @@
 const Recent = () => {
     return (
-        <div>
+        <div className='recent'>
             <h3 className='recent-head'>Recent Activity</h3>
-            <table className='recent-table'>
-                <tr>
-                    <th>Date</th>
-                    <th>Description</th>
-                    <th>Amount</th>
-                </tr>
-                <tr>
-                    <td>1/1/21</td>
-                    <td>Sent</td>
-                    <td>N100.00</td>
-                </tr>
-                <tr>
-                    <td>2/2/21</td>
-                    <td>Recieved</td>
-                    <td>N200.00</td>
-                </tr>
-                <tr>
-                    <td>3/3/21</td>
-                    <td>Sent</td>
-                    <td>N300.00</td>
-                </tr>
-            </table>
+            <div>
+                <table className='recent-table'>
+                    <tr>
+                        <th>Date</th>
+                        <th>Description</th>
+                        <th>Amount</th>
+                    </tr>
+                    <tr>
+                        <td>1/1/21</td>
+                        <td>Sent</td>
+                        <td>N100.00</td>
+                    </tr>
+                    <tr>
+                        <td>2/2/21</td>
+                        <td>Recieved</td>
+                        <td>N200.00</td>
+                    </tr>
+                    <tr>
+                        <td>3/3/21</td>
+                        <td>Sent</td>
+                        <td>N300.00</td>
+                    </tr>
+                </table>
+            </div>
         </div>
     );
 };
